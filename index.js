b8eadb8ace69c1cd3645c05e98de68a258855c4b
audibleblink <virtualsnyper@gmail.com>
2015-02-16 11:16:30 -0500

    remove socket listeners from server definition
    - rename static server module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http   = require('http'),
     fs     = require('fs'),
-    static = require("node-static"),
-    files  = new static.Server("./public"),
+    nStatic = require("node-static"),
+    files  = new nStatic.Server("./public"),
     port   = Number(process.env.PORT || 5005),
     fb     = require('firebase'),
     fBase  = new fb("https://blazing-heat-7747.firebaseio.com/members")
@@ -13,32 +13,31 @@ var server = http.createServer(function(req, res) {
         files.serve(req, res)
     }).resume()
 
-    io.on("connection", function(socket){
-        console.log("client connected")
-
-        fBase.on("value", function(snap){
-            var players = []
-            snap.forEach(function(snapItem) {
-                var player = snapItem.val()
-                player.key = snapItem.key()
-                players.push(player)      
-            })
-
-            socket.emit("fbChange", players)
-        })
-
-        socket.on("gamer added", function(gamer){
-            fBase.push(gamer)
-        })
-
-        socket.on("disconnect", function(){
-        })
-    })
-    
-
 })
 
 var io = require('socket.io')(server)
+
+io.on("connection", function(socket){
+    console.log("client connected")
+
+    fBase.on("value", function(snap){
+        var players = []
+        snap.forEach(function(snapItem) {
+            var player = snapItem.val()
+            player.key = snapItem.key()
+            players.push(player)      
+        })
+
+        socket.emit("fbChange", players)
+    })
+
+    socket.on("gamer added", function(gamer){
+        fBase.push(gamer)
+    })
+
+    socket.on("disconnect", function(){
+    })
+})
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
