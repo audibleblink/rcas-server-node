1f8616d4a11b226d59303c9622674c32f1c910ce
audibleblink <virtualsnyper@gmail.com>
2015-02-16 23:08:12 -0500

    better variable names

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
 var http   = require('http'),
-    fs     = require('fs'),
-    nStatic = require("node-static"),
-    files  = new nStatic.Server("./public"),
-    port   = Number(process.env.PORT || 5005),
+    statik = require("node-static"),
     fb     = require('firebase'),
     fBase  = new fb("https://blazing-heat-7747.firebaseio.com/members")
 
 
 var server = http.createServer(function(req, res) {
+
+    assets  = new statik.Server("./public")
     
     req.addListener("end", function(){
-        files.serve(req, res)
+        assets.serve(req, res)
     }).resume()
 
 })
 
+
 var io = require('socket.io')(server)
 
-io.on("connection", function(socket){
+io.on("connection", function(clientSocket){
     console.log("client connected")
 
-    fBase.on("value", function(snap){
+
+    fBase.on("value", function(snapshot){
         var players = []
-        snap.forEach(function(snapItem) {
+        snapshot.forEach(function(snapItem) {
             var player = snapItem.val()
             player.key = snapItem.key()
             players.push(player)      
         })
 
-        socket.emit("fbChange", players)
+        clientSocket.emit("fbChange", players)
     })
 
-    socket.on("gamer added", function(gamer){
+
+    clientSocket.on("gamer added", function(gamer){
         fBase.push(gamer)
     })
 
-    socket.on("gamer deleted", function(obj){
+
+    clientSocket.on("gamer deleted", function(obj){
         console.log("deleting player with key: " + obj.key)
         fBase.child(obj.key).remove()
     })
 })
+
+var port = Number(process.env.PORT || 5005)
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
